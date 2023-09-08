8c0de55a68b7fcf3e5776763d06cf3e55b8fff3f
Abiodun Olunu <abiodunolunu@gmail.com>
2023-09-08 12:35:50 +0100

    3.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import AwardFilled from './icon/AwardFilled.vue';
 import Vue3StarRatings from "./lib/components/Vue3StarRatings.vue";
 
 const rate = ref(2.5456789678248984728);
@@ -13,7 +12,7 @@ const rate = ref(2.5456789678248984728);
       place-content: center;
       height: 100vh;
     ">
-    <Vue3StarRatings v-model="rate" :number-of-stars="5" :star-size="24" :custom-Svg="AwardFilled" />
+    <Vue3StarRatings v-model="rate" :number-of-stars="5" :star-size="24" />
   </div>
 </template>
 
